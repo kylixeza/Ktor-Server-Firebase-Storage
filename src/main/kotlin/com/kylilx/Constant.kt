9a4eef18944f4935b5cfaeb3f9c2c62fae5a767b
ACER <loilyxtra@gmail.com>
2022-12-28 21:50:08 +0700

    added documentation

--- src/main/kotlin/com/kylilx/Constant.kt
+++ src/main/kotlin/com/kylilx/Constant.kt
@@ -1,5 +1,10 @@
 package com.kylilx
 
 object Constant {
+    /**
+     * based on your project name in firebase
+     * example: project name was Hello-World
+     * so storage bucket must be "hello-world.appspot.com"
+     */
     const val STORAGE_BUCKET = "ktor-firebase-storage.appspot.com"
 }
