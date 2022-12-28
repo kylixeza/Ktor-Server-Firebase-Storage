600fd52d0eb0714805e2e494644a9c4a074e22c0
ACER <loilyxtra@gmail.com>
2022-12-28 21:44:40 +0700

    added firebase admin dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,4 +26,7 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
+    // Firebase
+    implementation("com.google.firebase:firebase-admin:9.1.1")
 }
