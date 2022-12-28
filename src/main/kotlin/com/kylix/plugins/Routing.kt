467e30ba6d853c6bd151436f207cbd38cc959a34
ACER <loilyxtra@gmail.com>
2022-12-28 21:53:07 +0700

    added route

--- src/main/kotlin/com/kylix/plugins/Routing.kt
+++ src/main/kotlin/com/kylix/plugins/Routing.kt
@@ -1,14 +1,77 @@
 package com.kylix.plugins
 
+import com.google.firebase.cloud.StorageClient
+import com.kylix.FirebaseStorageUrl
+import com.kylix.FirebaseStorageUrl.getDownloadUrl
+import com.kylix.FirebaseStorageUrl.reference
+import io.ktor.http.*
+import io.ktor.http.content.*
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
+import java.util.*
 
 fun Application.configureRouting() {
+
+    val bucket = StorageClient.getInstance().bucket()
 
     routing {
         get("/") {
             call.respondText("Hello World!")
         }
+
+        post("/add-image") {
+            //upload image from multipart
+            val multipart = call.receiveMultipart()
+            var urlPath = ""
+
+            try {
+                multipart.forEachPart { part ->
+                    if (part is PartData.FileItem) {
+                        val (fileName, fileBytes) = part.convert()
+                        bucket.create(fileName, fileBytes, "image/png")
+                        urlPath = FirebaseStorageUrl
+                            .createPath()
+                            .getDownloadUrl(fileName)
+                    }
+                }
+                call.respondText(urlPath)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                call.respond(HttpStatusCode.BadRequest, "Error while uploading image")
+            }
+        }
+
+        post("/add-image-with-path") {
+            //upload image from multipart
+            val multipart = call.receiveMultipart()
+            var urlPath = ""
+
+            try {
+                multipart.forEachPart { part ->
+                    if (part is PartData.FileItem) {
+                        val (fileName, fileBytes) = part.convert()
+                        bucket.create("avatar_url/images/$fileName", fileBytes, "image/png")
+                        urlPath = FirebaseStorageUrl
+                            .createPath()
+                            .reference("avatar_url")
+                            .reference("images")
+                            .getDownloadUrl(fileName)
+                    }
+                }
+                call.respondText(urlPath)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                call.respond(HttpStatusCode.BadRequest, "Error while uploading image")
+            }
+        }
     }
+}
+
+fun PartData.FileItem.convert() = run {
+    val fileBytes = streamProvider().readBytes()
+    val fileExtension = originalFileName?.takeLastWhile { it != '.' }
+    val fileName = UUID.randomUUID().toString() + "." + fileExtension
+    Pair(fileName, fileBytes)
 }
