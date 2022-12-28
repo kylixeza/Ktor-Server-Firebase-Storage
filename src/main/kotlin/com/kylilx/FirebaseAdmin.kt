aa2dcf89c68a0d6e17481a70f841edfc7a76ac55
ACER <loilyxtra@gmail.com>
2022-12-28 21:48:27 +0700

    added firebase admin

---
+++ src/main/kotlin/com/kylilx/FirebaseAdmin.kt
@@ -0,0 +1,18 @@
+package com.kylilx
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import java.io.InputStream
+
+object FirebaseAdmin {
+    private val serviceAccount: InputStream? =
+        this::class.java.classLoader.getResourceAsStream("admin-key.json")
+
+    private val options: FirebaseOptions = FirebaseOptions.builder()
+        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+        .setStorageBucket(Constant.STORAGE_BUCKET)
+        .build()
+
+    fun init(): FirebaseApp = FirebaseApp.initializeApp(options)
+}
